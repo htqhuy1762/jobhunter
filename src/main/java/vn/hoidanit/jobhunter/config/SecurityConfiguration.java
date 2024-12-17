c2976102bd0888aa06adfe7b5e4be338be121a20
Huy Huynh <htqhuy1762@gmail.com>
2024-12-18 00:34:56 +0700

    Module Company (Not pagination)

--- src/main/java/vn/hoidanit/jobhunter/config/SecurityConfiguration.java
+++ src/main/java/vn/hoidanit/jobhunter/config/SecurityConfiguration.java
@@ -4,6 +4,7 @@ import javax.crypto.SecretKey;
 import javax.crypto.spec.SecretKeySpec;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.config.CustomEditorConfigurer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
---
+++ src/main/java/vn/hoidanit/jobhunter/service/CompanyService.java
@@ -0,0 +1,44 @@
+package vn.hoidanit.jobhunter.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
+
+import vn.hoidanit.jobhunter.domain.Company;
+import vn.hoidanit.jobhunter.repository.CompanyRepository;
+
+@Service
+public class CompanyService {
+    private final CompanyRepository companyRepository;
+
+    public CompanyService(CompanyRepository companyRepository) {
+        this.companyRepository = companyRepository;
+    }
+
+    public Company handleCreateCompany(Company c) {
+        return this.companyRepository.save(c);
+    }
+
+    public List<Company> handleGetCompany() {
+        return this.companyRepository.findAll();
+    }
+
+    public Company handleUpdateCompany(Company c) {
+        Optional<Company> companyOptional = this.companyRepository.findById(c.getId());
+        if(companyOptional.isPresent()) {
+            Company currentCompany = companyOptional.get();
+            currentCompany.setLogo(c.getLogo());
+            currentCompany.setName(c.getName());
+            currentCompany.setDescription(c.getDescription());
+            currentCompany.setAddress(c.getAddress());
+            return this.companyRepository.save(currentCompany);
+        }
+        return null;
+    }
+
+    public void handleDeleteCompany(long id) {
+        this.companyRepository.deleteById(id);
+    }
+
+}
