8f2942d28dc8d283b9010810bae38b8c9f209e9f
Huy Huynh <htqhuy1762@gmail.com>
2025-02-06 14:02:31 +0700

    Email service #1

---
+++ src/main/java/vn/hoidanit/jobhunter/service/EmailService.java
@@ -0,0 +1,47 @@
+package vn.hoidanit.jobhunter.service;
+
+import java.nio.charset.StandardCharsets;
+
+import org.springframework.mail.MailException;
+import org.springframework.mail.MailSender;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Service;
+
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
+
+@Service
+public class EmailService {
+    private final MailSender mailSender;
+    private final JavaMailSender javaMailSender;
+
+    public EmailService(MailSender mailSender, JavaMailSender javaMailSender) {
+        this.mailSender = mailSender;
+        this.javaMailSender = javaMailSender;
+    }
+
+    public void sendSimpleEmail() {
+        SimpleMailMessage msg = new SimpleMailMessage();
+        msg.setTo("huysirox365@gmail.com");
+        msg.setSubject("Test email from spring boot");
+        msg.setText("Hello world");
+        this.mailSender.send(msg);
+    }
+
+    public void sendEmailSync(String to, String subject, String content, boolean isMultipart, boolean isHtml) {
+        // Prepare message using a Spring helper
+        MimeMessage mimeMessage = this.javaMailSender.createMimeMessage();
+        try {
+            MimeMessageHelper message = new MimeMessageHelper(mimeMessage, isMultipart, StandardCharsets.UTF_8.name());
+            message.setTo(to);
+            message.setSubject(subject);
+            message.setText(content, isHtml);
+            this.javaMailSender.send(mimeMessage);
+        } catch (MailException | MessagingException e) {
+            System.out.println("ERROR SEND EMAIL: " + e);
+        }
+    }
+
+}
