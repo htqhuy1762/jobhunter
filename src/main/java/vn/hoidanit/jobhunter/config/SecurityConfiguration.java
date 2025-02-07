a9b2435608fa1fca38ff487553665727c42b61b1
Huy Huynh <htqhuy1762@gmail.com>
2025-02-07 13:42:27 +0700

    add Swagger API Document

--- src/main/java/vn/hoidanit/jobhunter/config/SecurityConfiguration.java
+++ src/main/java/vn/hoidanit/jobhunter/config/SecurityConfiguration.java
@@ -78,7 +78,10 @@ public class SecurityConfiguration {
         String[] whiteList = {
                 "/",
                 "/api/v1/auth/login", "/api/v1/auth/refresh", "/api/v1/auth/register", "/storage/**",
-                "/api/v1/companies/**", "/api/v1/jobs/**", "/api/v1/email/**",
+                "/api/v1/companies/**", "/api/v1/jobs/**", "/api/v1/email/**", "/v3/api-docs/**",
+                "/swagger-ui/**",
+                "/swagger-ui.html"
+
         };
         http
                 .csrf(c -> c.disable())
